c9db034e80d583b2e3f7314b82525835d653ab46
topochoverde <topochoverdem@gmail.com>
2024-05-01 21:52:57 -0400

    Creado archivo de prueba
    Se crea archivo para poder hacer pruebas y visualizar la forma en las que se ven los cambios en los commits

---
+++ archivo de prueba.R
@@ -0,0 +1,7 @@
+#Sympols de prueba, valores aleatoreos.
+  x<-3
+class(x)
+
+  y<-c(1,2,3,4)
+matrix(y, nrow=2, ncol = 2 )
+#holis (espero que los comits se puedan borrar para que no se enbasure el proteyecto)
